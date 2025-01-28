a37286aec35efd2f71b2d6fc265c42a8dca72bcb
Cleber-Canto <cantosaraiva@hotmail.com>
2025-01-28 10:58:35 -0400

    Corrigir fechamento do servidor e ajustes na configuração

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,8 +1,20 @@
 import { SetupServer } from '@src/server';
-import supertest, { SuperTest, Test } from 'supertest';
+import supertest from 'supertest';
 
-beforeAll(() => {
-  const server = new SetupServer();
-  server.init();
-  global.testRequest = supertest(server.getApp()) as unknown as SuperTest<Test>;
+let server: SetupServer;
+
+beforeAll(async () => {
+  server = new SetupServer();
+  await server.init();
+  global.testRequest = supertest(server.getApp()) as unknown as supertest.SuperTest<supertest.Test>;
+});
+
+afterAll(async () => {
+  try {
+    if (server) {
+      await server.close(); // Fecha o servidor e libera recursos sem logs desnecessários
+    }
+  } catch (error) {
+    console.error('Erro ao fechar o servidor:', error); // Apenas loga o erro se ocorrer
+  }
 });
